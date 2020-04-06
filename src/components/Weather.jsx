20ac0e1f0acf4d03116d69cd9eec1961b9f4b4d6
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-06 18:44:35 -0400

    connected weather api

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -12,7 +12,7 @@ class Weather extends Component {
     }
 
     componentDidMount() {
-        axios.get("api.openweathermap.org/data/2.5/weather?id=2172797&appid=736dcd581cc9d13aca218c4fc55cade8")
+        axios.get("https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=736dcd581cc9d13aca218c4fc55cade8")
             .then(res => {
                 console.log(res.data)
             })
